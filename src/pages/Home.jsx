e6b0ecb3d326edce6e420ed42ce1956c80d21b74
babu4prasad <babumukesh8743@gmail.com>
2024-01-08 13:37:03 +0530

    fifth commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from "../Components/Header";
+
+
+
+
+    
+    export default function Home() {
+      return (
+        <>
+            <Header />
+            <h3>HOME PAGE</h3>
+
+        </>
+      )
+    }
+    
+    
---
+++ src/pages/Services.jsx
@@ -0,0 +1,17 @@
+import Header from "../Components/Header";
+
+
+
+
+    
+    export default function Services() {
+      return (
+        <>
+            <Header />
+            <h3>SERVICE PAGE</h3>
+
+        </>
+      )
+    }
+    
+    
